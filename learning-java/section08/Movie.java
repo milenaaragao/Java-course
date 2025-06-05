ed29620b5d6233e22361ddc9583396f47bc4f30e
Milena Aragão <50327608+milenaaragao@users.noreply.github.com>
2025-06-05 20:53:40 +0100

    Create Movie.java
    Learning Polymorphism

---
+++ learning-java/section08/Movie.java
@@ -0,0 +1,157 @@
+/////////////////////////////// MAIN CLASS ///////////////////////////////////
+
+import java.util.Scanner;
+
+public class Main {
+
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter Type (A for Adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q to quit): ");
+            String type = s.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+    }
+}
+
+
+//////////////////////////// NextMain /////////////////////////////////////////
+
+
+public class NextMain {
+
+    public static void main(String[] args) {
+
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+
+        Object unknownObject = Movie.getMovie("S", "Star Wars");
+        if (unknownObject.getClass().getSimpleName() == "Comedy") {
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if (unknownObject instanceof Adventure) {
+            ((Adventure) unknownObject).watchAdventure();
+        } else if (unknownObject instanceof ScienceFiction syfy) {
+            syfy.watchScienceFiction();
+        }
+    }
+}
+
+
+///////////////////////////// Movie ////////////////////////////////////////////
+
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+
+    public static Movie getMovie(String type, String title) {
+
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+}
+
+class Adventure extends Movie {
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+
+    public void watchAdventure() {
+        System.out.println("Watching an Adventure!");
+    }
+}
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy Ending");
+    }
+
+    public void watchComedy() {
+        System.out.println("Watching a Comedy!");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Bad Aliens do Bad Stuff",
+                "Space Guys Chase Aliens",
+                "Planet Blows Up");
+    }
+
+    public void watchScienceFiction() {
+        System.out.println("Watching a Science Fiction Thriller!");
+    }
+
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
